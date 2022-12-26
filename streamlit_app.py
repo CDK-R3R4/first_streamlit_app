a4eb8cf2715e8843d3e9542f675e86f310ff47b1
CDK-R3R4 <112666027+CDK-R3R4@users.noreply.github.com>
2022-12-26 10:11:34 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,6 +21,8 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_selected)
 
 #Nueva seccion para mostrar el response de la api fruityvice
+streamlit.header("Fruityvice Fruit Advice!")
+
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
+streamlit.text(fruityvice_response.json())
